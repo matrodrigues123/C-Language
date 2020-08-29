c30f979fe9822a2105a70211c9a85ef0cf3d4f13
matrodrigues123 <63891336+matrodrigues123@users.noreply.github.com>
2020-08-29 08:52:04 -0300

    qaaa

--- CES 10/Lab 05/Matheus_Rodrigues5.cpp
+++ CES 10/Lab 05/Matheus_Rodrigues5.cpp
@@ -11,43 +11,48 @@
 
 int main()
 {
-
     //Declaração de variáveis
-    int i,j,k,cont; //Variáveis referentes à contagem
-    int quant; //Variável referente ao retorno de scanf dos coeficientes
-    int quantTeste; //Variável referente ao retorno de scanf do teste 
-    char pontoVirg; //Variável presente apenas para detectar se há ponto e vírgula
-    char operacao;
-    float coef[8] = {0}; 
-    float sumCoef[8] = {0};
-    float derivada[8] = {0};
-    float teste; //Armazena o float apenas para testar se há um novo polinômio
-    float valorX; 
-    float valorPol;
-    float maiorExpo;
+    
+    int i,k,cont, //Variáveis referentes à contagem
+        quant, //Variável referente ao retorno de scanf dos coeficientes
+        quantTeste; //Variável referente ao retorno de scanf do teste 
 
-    //Declaração das variáveis e explicitação do caminho para os arquivos de entrada e saída
-    FILE * entrada;
-    FILE * saida;
+    char pontoVirg, 
+         operacao;
+
+    float entCoef[8] = {0}, //Vetor que armazena os coeficientes da entrada
+          resultCoef[8] = {0}, //Vetor que armazena o coeficientes do polinômio resultante das operações
+          teste, //Armazena o float apenas para testar se há um novo polinômio
+          valorX, 
+          valorPol,
+          maiorExpo;
+
+    FILE * entrada,
+         * saida;
+
+
+    //Explicitação do caminho para os arquivos de entrada e saída
     entrada = fopen("C:\\Lab5\\entrada5.txt", "r");
     saida = fopen("C:\\Lab5\\Matheus_Rodrigues5.txt", "w");
     
+
     //Inicialização de algumas variáveis
+    i = 0;
     cont = 1;
     quant = 1;
     quantTeste = 1;
-    i = 0;
+    pontoVirg = ';';
     valorPol = 0;
     maiorExpo = 0;
-    pontoVirg = ';';
     
+
     //Identificar o tipo de operação
     fscanf (entrada, "%c", &operacao);
+
 
     //Enquanto não for identificado o 'F' no arquivo de entrada, fazer
     while (operacao != 'F')
     {
-        
         //Para a operação de soma, fazer
         if (operacao == 's' || operacao == 'S')
         {
@@ -57,98 +62,111 @@ int main()
                 //Armazena os coeficientes do polinômio até alcançar o ponto e vírgula
                 while (quant > 0)
                 {
-                    quant = fscanf (entrada, " %f", &coef[i]);
-                    sumCoef[i] += coef[i];
-                    coef[i] = 0;
+                    quant = fscanf (entrada, " %f", &entCoef[i]);
+                    resultCoef[i] += entCoef[i];
+                    entCoef[i] = 0;
+
                     //Garante que o grau máximo do polinômio é 7
                     if (i < 7)
                     {
                         i++;
                     }
-                    
                 }
+
 
                 //Verifica se há ponto e vírgula seguido de float
                 fscanf (entrada, " %c", &pontoVirg);
                 quantTeste = fscanf (entrada, "%f", &teste);
 
+
                 if (quantTeste == 1)
                 {
-                    sumCoef[0] += teste; //Casa haja um próximo polinômio, seu 'coef[0]' já foi armazenado em 'teste'
+                    resultCoef[0] += teste; //Casa haja um próximo polinômio, seu 'coef[0]' já foi armazenado em 'teste'
                     i = 1; //O loop anterior deve ser repetido a partir do 'coef[1]'
-                    quant = 1;
                 }
                 else
                 {
                     i = 0;
-                    quant = 1;
                 }
-                
+
+
+                //Reinicialização da variável
+                quant = 1;
             }
             
+
             fprintf (saida,"%3d: ",cont);
+
 
             //Escreve a resposta no arquivo de saída
             for (k = 8; k >= 0; k--)
             {
                 //Verifica se o termo analisado é o de maior expoente do polinômio
                 //'maiorExpo' é sempre inicializada com o grau 0
-                if (k > maiorExpo && sumCoef[k] != 0)
+                if (k > maiorExpo && resultCoef[k] != 0)
                 {
                     maiorExpo = k;
                 }
                 //Garante que no arquivo de saída só aparece os termos com coeficiente diferente de zero
-                if (sumCoef[k] != 0)
+                if (resultCoef[k] != 0)
                 {
                     //Garante que o termo de maior expoente vem primeiro e não acompanhado de '+', caso seja positivo
+                    //Caso seja negativo, garante espaçamento adequado
                     if (k == maiorExpo)
                     {
-                        switch (k)
+                        if (resultCoef[k] > 0)
                         {
-                            case 0:
-                                fprintf (saida, "%g",sumCoef[k]);
-                                break;
+                            switch (k)
+                            {
+                                case 0:
+                                    fprintf (saida, "%g",resultCoef[k]);
+                                    break;
 
-                            case 1:
-                                fprintf (saida, "%gx",sumCoef[k]);
-                                break;
-                            
-                            default:
-                                fprintf (saida, "%gx^%d",sumCoef[k],k);
-                                break;
+                                case 1:
+                                    fprintf (saida, "%gx",resultCoef[k]);
+                                    break;
+                                
+                                default:
+                                    fprintf (saida, "%gx^%d",resultCoef[k],k);
+                                    break;
+                            }
+                        }
+                        else
+                        {
+                            fprintf (saida, "- %gx^%d",-resultCoef[k],k);
                         }
                     }
-                    else if (sumCoef[k] > 0)
+                    else if (resultCoef[k] > 0)
                     {
                         switch (k)
                         {
                             case 0:
-                                fprintf (saida, " + %g",sumCoef[k]);
+                                fprintf (saida, " + %g",resultCoef[k]);
                                 break;
 
                             case 1:
-                                fprintf (saida, " + %gx",sumCoef[k]);
+                                fprintf (saida, " + %gx",resultCoef[k]);
                                 break;
                             
                             default:
-                                fprintf (saida, " + %gx^%d",sumCoef[k],k);
+                                fprintf (saida, " + %gx^%d",resultCoef[k],k);
                                 break;
                         }
                     }
-                    //Garante espaçamento adequado para o sinal em coeficientes negativos
                     else
                     {
-                        fprintf (saida, " - %gx^%d",-sumCoef[k],k);
+                        fprintf (saida, " - %gx^%d",-resultCoef[k],k);
                     }
                     
                 }
                 
             }
             //Escreve zero no arquivo de saída para o polinômio nulo
-            if (maiorExpo == 0 && sumCoef[0] == 0)
+            if (maiorExpo == 0 && resultCoef[0] == 0)
             {
                 fprintf (saida, "0");
             }
+
 
             //Variáveis reinicializadas em preparação para o próximo loop e contagem
             i = 0;
@@ -157,102 +175,113 @@ int main()
             quantTeste = 1;
             for (k = 0; k < 8; k++)
             {
-                sumCoef[k] = 0;
+                resultCoef[k] = 0;
             }
             cont++;
+
 
             fprintf (saida, "\n");
         }
 
-        //Para a operação de derivada, fazer
+        //Para a operação de resultCoef, fazer
         if (operacao == 'd' || operacao == 'D')
         {
             while (operacao != ';')
             {
                 //Armazenar os coeficientes do polinomio até chegar no ponto e vírgula
-                //Para cada termo, obtém-se a derivada
-                quant = fscanf (entrada,"%f",&coef[i]);
-                derivada[i] = i * coef[i];
+                //Para cada termo, obtém-se a resultCoef
+                quant = fscanf (entrada,"%f",&entCoef[i]);
+                resultCoef[i] = i * entCoef[i];
                 
-                coef[i] = 0;
+                entCoef[i] = 0;
                 i++;
 
                 fscanf (entrada, "%c", &operacao);
                  
             }
 
-            //Garante que o termo de expoente 7 não apareça na derivada
-            derivada[8] = 0;
+
+            //Garante que o termo de expoente 7 não apareça na resultCoef
+            resultCoef[8] = 0;
+
 
             fprintf (saida, "%3d: ", cont);
+
+
             //Análogo ao caso anterior
             for (k = 8; k >= 0; k--)
             {
-                if (k > maiorExpo && derivada[k] != 0)
+                if (k > maiorExpo && resultCoef[k] != 0)
                 {
                     maiorExpo = k;
                 }
                 
-                if (derivada[k] != 0)
+                if (resultCoef[k] != 0)
                 {
-                    //A operação é efetuada com 'k - 1' ao invés de 'k', pela condição da derivada
+                    //A operação é efetuada com 'k - 1' ao invés de 'k', pela condição da resultCoef
                     if (k == maiorExpo)
                     {
-                        switch (k - 1)
+                        if (resultCoef [k] > 0)
                         {
-                            case 0:
-                                fprintf (saida, "%g",derivada[k]);
-                                break;
+                                switch (k - 1)
+                            {
+                                case 0:
+                                    fprintf (saida, "%g",resultCoef[k]);
+                                    break;
 
-                            case 1:
-                                fprintf (saida, "%gx",derivada[k]);
-                                break;
-                            
-                            default:
-                                fprintf (saida, "%gx^%d",derivada[k],k - 1);
-                                break;
+                                case 1:
+                                    fprintf (saida, "%gx",resultCoef[k]);
+                                    break;
+                                
+                                default:
+                                    fprintf (saida, "%gx^%d",resultCoef[k],k - 1);
+                                    break;
+                            }
+                        }
+                        else
+                        {
+                            fprintf (saida, "- %gx^%d",-resultCoef[k],k - 1);
                         }
                     }
-                    else if (derivada[k] > 0)
+                    else if (resultCoef[k] > 0)
                     {
                         switch (k - 1)
                         {
                             case 0:
-                                fprintf (saida, " + %g",derivada[k]);
+                                fprintf (saida, " + %g",resultCoef[k]);
                                 break;
 
                             case 1:
-                                fprintf (saida, " + %gx",derivada[k]);
+                                fprintf (saida, " + %gx",resultCoef[k]);
                                 break;
                             
                             default:
-                                fprintf (saida, " + %gx^%d",derivada[k],k - 1);
+                                fprintf (saida, " + %gx^%d",resultCoef[k],k - 1);
                                 break;
                         }
                     }
                     else
                     {
-                        fprintf (saida, " - %gx^%d",-derivada[k],k - 1);
+                        fprintf (saida, " - %gx^%d",-resultCoef[k],k - 1);
                     }
                     
                 }
-                
-                
             }
-
-            if (maiorExpo == 0 && derivada[0] == 0)
+            if (maiorExpo == 0 && resultCoef[0] == 0)
             {
                 fprintf (saida, "0");
             }
+
 
             //Variáveis reinicializadas em preparação para o próximo loop e contagem
             i = 0;
             maiorExpo = 0;
             for (k = 0; k < 8; k++)
             {
-                derivada[k] = 0;
+                resultCoef[k] = 0;
             }
             cont++;
+
 
             fprintf (saida, "\n");
         }
@@ -263,7 +292,7 @@ int main()
             //Armazena os coeficientes do polinômio até chegar no ponto e vírgula
             while (operacao != ';')
             {
-                quant = fscanf (entrada,"%f",&coef[i]);
+                quant = fscanf (entrada,"%f",&entCoef[i]);
                 i++;
                 fscanf (entrada, "%c", &operacao);
             }
@@ -272,34 +301,38 @@ int main()
             //Armazena o valor a ser substituido no polinômio
             quant = fscanf (entrada, "%f", &valorX);
 
+
             //Calcula o valor do polinômio no ponto obtido anteriormente
-            for (j = i; j >= 0; j--)
+            for (k = i; k >= 0; k--)
             {
-                valorPol += coef[j] * pow (valorX,j);
-                coef[j] = 0;
+                valorPol += entCoef[k] * pow (valorX,k);
+                entCoef[k] = 0;
             }
+
 
             //Escreve a resposta no arquivo de saída
             fprintf (saida, "%3d: ",cont);
             fprintf (saida, "%g",valorPol);
             
+
             //Variáveis reinicializadas em preparação para o próximo loop e contagem
             i = 0;
             valorPol = 0;
             maiorExpo = 0;
             cont++;
 
+
             fprintf (saida, "\n");
         }
         
         
         fscanf (entrada, "%c", &operacao);
-
     }
 
     //Fecha os arquivos de entrada e saída
     fclose (entrada);
     fclose (saida);
+
 
     //Pausa + fim do programa
     system ("pause");
---
+++ CES 10/Lab 06/Matheus_Rodrigues6.cpp
@@ -0,0 +1,168 @@
+/*  Matheus Rodrigues Araújo                        */
+/*  Turma 24.1                                      */
+/*  Exercício 6 : Strings                           */
+/*                                                  */
+/*  Programa compilado com Code Blocks 17.12        */
+
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+
+#define FALSE 0
+#define TRUE 1
+#define boolean int
+
+int main()
+{
+    //Declaração de variáveis
+    char localArq[30],
+         localArqAnt[30],
+         nomeArq[15],
+         nomeArqAnt[15],
+         palavraEnt[15],
+         palavra[15],
+         marcas [80]; //char que armazena as marcações no arquivo, como "--" e "|"
+    
+    int cont = 0,
+        i;
+
+    boolean rptLoc = FALSE, //booleans referentes à repetição de local e nome do arquivo
+            rptNom = FALSE;
+
+    FILE * entrada,
+         * saida,
+         * local;
+    
+
+    //Abertura dos arquivos de entrada e saída
+    entrada = fopen("C:\\Lab6\\entrada6.txt", "r");
+    saida = fopen("C:\\Lab6\\Matheus_Rodrigues6.txt", "w");
+
+
+    //Pula as primeiras linhas
+    for (i = 0; i < 7; i++)
+    {
+        fgets (marcas, 80, entrada);
+    }
+
+
+    //Escreve as mensagens iniciais no arquivo
+    fprintf (saida," Esse é o resultado da contagem\n");
+    fprintf (saida," O programa lê um arquivo de entrada contendo o local e nome do arquivo\n");
+    fprintf (saida," Para palavras em um mesmo local e arquivo, não é repetido o endereço na saída.\n");
+    fprintf (saida," ----------------------------------------\n");
+
+    //Armazena o "|" para que o próximo string lido seja relevante 
+    //Aparece várias vezes com mesma finalidade
+    fscanf (entrada, "%s", &marcas);
+
+    //Enquanto não for identificado o próximo traço no arquivo, fazer
+    while (strcmp(marcas, "---------------------------------------------------------------") != 0)
+    {
+        //Leitura da localização do arquivo
+        fscanf (entrada, "%s", localArq);
+
+        //Faz a variável 'Ant' assumir o valor anteriormente armazenado 
+        //e faz o boolean assumir "TRUE" caso 'localArq' seja lido como "=" ou esteja repetido
+        if (strcmp(localArq, "=") == 0 || strcmp(localArq, localArqAnt) == 0) 
+        {
+            rptLoc = TRUE;
+            strcpy (localArq, localArqAnt);
+        }
+
+        fscanf (entrada, "%s", marcas);
+
+        //Leitura do nome do arquivo 
+        fscanf(entrada, "%s", nomeArq);
+
+        //Análogo ao anterior
+        if (strcmp(nomeArq, "=") == 0 || strcmp (nomeArq, nomeArqAnt) == 0)
+        {
+            rptNom = TRUE;
+            strcpy (nomeArq, nomeArqAnt);
+        }
+
+        fscanf (entrada, "%s", marcas);
+
+        //Leitura da palavra a ser buscada
+        fscanf(entrada, "%s", palavraEnt);
+
+        fscanf (entrada, "%s", marcas);
+
+        //Armazena o último string lido nas variáveis 'Ant'
+        strcpy (localArqAnt, localArq);
+        strcpy (nomeArqAnt, nomeArq);
+
+        //Estruturação do caminho do arquivo
+        strcat (localArq,"\\");
+        strcat (localArq,nomeArq);
+        strcat (localArq,".txt");
+
+        //Abertura do local do arquivo previamente estruturado
+        local = fopen(localArq, "r");
+
+        //Verifica se o endereço referente a 'local' é válido
+        if (local == NULL)
+        {
+            fprintf (saida, "\n");
+            fprintf (saida, " %s\n", localArq);
+            fprintf (saida, "             ARQUIVO NAO ENCONTRADO.\n");
+
+            //Fecha o arquivo em preparação para o próximo loop
+            fclose (local);
+
+            //Reinicialização
+            cont = 0;
+            rptLoc = FALSE;
+            rptNom = FALSE;
+
+            fscanf (entrada, "%s", marcas); 
+        }
+        else
+        {
+            //Verifica se houve repetição de endereço,
+            //garantindo que este só é escrito no arquivo de saída uma vez
+            if (rptLoc == FALSE || rptNom == FALSE)
+            {
+                fprintf (saida, "\n");
+                fprintf (saida, " %s\n", localArq);
+            }
+
+            //Contagem da palavra no arquivo aberto
+            while (!feof(local))
+            {
+                fscanf (local,"%s ", palavra);
+                if (strcmp(palavra, palavraEnt) == 0)
+                {
+                    cont++;
+                }
+            }
+
+            //Fecha o arquivo em preparação para o próximo loop
+            fclose(local);
+            
+            //Escreve na saída a palavra e seu respectivo número de ocorrências
+            fprintf (saida, "             %s : %d ocorrencias\n", palavraEnt, cont);
+
+            //Reinicialização
+            cont = 0;
+            rptLoc = FALSE;
+            rptNom = FALSE;
+
+            fscanf (entrada, "%s", marcas);
+        }
+
+    }
+    
+    //Fecha todos os arquivos
+    fclose (entrada);
+    fclose (saida);
+    fclose (local);
+
+    //Pausa + finalização
+    system ("pause");
+    return 0;
+
+}
