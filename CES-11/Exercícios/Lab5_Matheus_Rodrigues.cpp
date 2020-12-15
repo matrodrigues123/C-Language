c3da36d1f7bf4895daafb2613f8c53388533ef0a
matrodrigues123 <63891336+matrodrigues123@users.noreply.github.com>
2020-12-15 18:32:47 -0300

    asdsa

--- CES-11/Exercícios/Lab5_Matheus_Rodrigues.cpp
+++ CES-11/Exercícios/Lab5_Matheus_Rodrigues.cpp
@@ -5,9 +5,8 @@ typedef int vertice;
 typedef struct CelulaAdj CelulaAdj;
 typedef struct CelulaVertice CelulaVertice;
 typedef struct Grafo Grafo;
-typedef int logic;
-typedef char boolean;
-const boolean TRUE = 1, FALSE = 0;
+typedef char logic;
+const logic TRUE = 1, FALSE = 0;
 
 struct CelulaAdj {
 	vertice vert;
@@ -26,16 +25,31 @@ struct Grafo {
 	logic aciclico;
 };
 
+typedef struct noh noh;
+typedef noh *pilha;
+struct noh {vertice elem; noh *prox;};
+
 
 /*	 Variaveis globais */
 
 Grafo G;
 FILE *filein;
+int cont; pilha P; logic aciclico;pilha topol;
+
 
 /*		Prototipos de funcoes	*/
 
 void LerGrafo (Grafo *);
 void EscreverGrafo (Grafo *);
+void Empilhar (vertice, pilha*);
+void Desempilhar (pilha*);
+vertice Topo (pilha);
+void InicPilha (pilha*);
+logic Vazia (pilha);
+void Travessia (Grafo*);
+logic Procurar (vertice, pilha*);
+void BuscaProf (vertice, Grafo*);
+void OrdTopologica (pilha);
 
 /*		Programa Principal:  	*/
 
@@ -47,13 +61,25 @@ int main () {
 	LerGrafo (&G);
 	printf ("\nGrafo G em fase inicial\n");
 	EscreverGrafo (&G);
+	Travessia (&G);
+	if (aciclico == TRUE)
+	{
+		printf ("Aciclico\n");
+		printf ("Ordem topologica: ");
+		OrdTopologica(topol);
+	}
+	else
+	{
+		printf ("Ciclico");
+	}
+	
   	printf ("\n\n"); system ("pause"); return 0;
 }
 
 void LerGrafo (Grafo *G) {
 	vertice vert1, vert2;
-	CelulaAdj *pont;
-	boolean repetido = FALSE;
+	CelulaAdj *pontAdj, *pontContra, *aux;
+	logic repetido = FALSE;
 	int i;
 
 	fscanf (filein, "%d", &G->nvert);
@@ -64,6 +90,8 @@ void LerGrafo (Grafo *G) {
 		G->EspacoVertices[i].visit = -1;
 		G->EspacoVertices[i].listadj = (CelulaAdj *) malloc (sizeof(CelulaAdj));
 		G->EspacoVertices[i].listadj->prox = NULL;
+		G->EspacoVertices[i].listcontradj = (CelulaAdj *) malloc (sizeof(CelulaAdj));
+		G->EspacoVertices[i].listcontradj->prox = NULL;
 	}
 	G->aciclico = -1;
 
@@ -72,20 +100,31 @@ void LerGrafo (Grafo *G) {
 	while (vert1 != 0)
 	{
 		fscanf (filein, "%d", &vert2);
-		pont = G->EspacoVertices[vert1].listadj;
+		pontAdj = G->EspacoVertices[vert1].listadj;
 
 		if (vert1 > 0 && vert1 <= G->nvert && vert2 > 0 && vert2 <= G->nvert)
 		{
-			while (pont->prox != NULL)
+			while (pontAdj->prox != NULL)
 			{
-				if (pont->vert == vert2) repetido = TRUE;
-				pont = pont->prox;
+				if (pontAdj->vert == vert2) repetido = TRUE;
+				pontAdj = pontAdj->prox;
 			}
 			if (repetido == FALSE)
 			{
-				pont->vert = vert2;
-				pont->prox = (CelulaAdj *) malloc (sizeof(CelulaAdj));
-				pont->prox->prox = NULL;
+				pontAdj->vert = vert2;
+
+				// Contra adjacência
+				pontContra = G->EspacoVertices[vert2].listcontradj;
+				while (pontContra->prox != NULL)
+				{
+					pontContra = pontContra->prox;
+				}
+				pontContra->vert = vert1;
+				pontContra->prox = (CelulaAdj *) malloc (sizeof(CelulaAdj));
+				pontContra->prox->prox = NULL;
+				
+				pontAdj->prox = (CelulaAdj *) malloc (sizeof(CelulaAdj));
+				pontAdj->prox->prox = NULL;
 			}
 		}
 		else
@@ -99,24 +138,150 @@ void LerGrafo (Grafo *G) {
 		repetido = FALSE;
 	}
 
+	// Dar free nas sobras
+	for (i = 1; i <= G->nvert; i++)
+	{
+		pontAdj = G->EspacoVertices[i].listadj;
+		if (pontAdj->prox != NULL)
+		{
+			while (pontAdj->prox != NULL)
+			{
+				aux = pontAdj;
+				pontAdj = pontAdj->prox;
+			}
+			aux->prox = pontAdj->prox;
+			free(pontAdj);
+		}
+		else
+		{
+			free(pontAdj);
+			G->EspacoVertices[i].listadj = NULL;
+		}
+
+		pontContra = G->EspacoVertices[i].listcontradj;
+		if (pontContra->prox != NULL)
+		{
+			while (pontContra->prox != NULL)
+			{
+				aux = pontContra;
+				pontContra = pontContra->prox;
+			}
+			aux->prox = pontContra->prox;
+			free(pontContra);
+		}
+		else
+		{
+			free(pontContra);
+			G->EspacoVertices[i].listcontradj = NULL;
+		}
+	}
+	
+
 }
 
 void EscreverGrafo (Grafo *G){
 	int i;
-	CelulaAdj *pont;
+	CelulaAdj *pontAdj, *pontContra;
 
 	for (i = 1; i <= G->nvert; i++)
 	{
 		printf ("Vertice %d)  ", i);
 		printf("Visitado: %3d; N.o de Visita: %3d;\n", G->EspacoVertices[i].visit, G->EspacoVertices[i].nvisit);
 		printf ("\tAdjacentes: ");
-		for (pont = G->EspacoVertices[i].listadj; pont->prox != NULL; pont = pont->prox)
+		for (pontAdj = G->EspacoVertices[i].listadj; pontAdj != NULL; pontAdj = pontAdj->prox)
 		{
-			printf ("%3d ", pont->vert);
+			printf ("%3d ", pontAdj->vert);
+		}
+		printf ("\n");
+		printf ("\tContra adjacentes: ");
+		for (pontContra = G->EspacoVertices[i].listcontradj; pontContra != NULL; pontContra = pontContra->prox)
+		{
+			printf ("%3d ", pontContra->vert);
 		}
 		printf ("\n\n");
 	}
 	
 }
+
+// Teste de aciclicidade
+
+void Empilhar (vertice x, pilha *P) { 
+    noh *temp;
+	temp = *P;  
+    *P = (noh *) malloc (sizeof (noh));
+	(*P)->elem = x; 
+    (*P)->prox = temp;
+}
+
+void Desempilhar (pilha *P) {
+	noh *temp; 
+	if (! Vazia(*P)) 
+    {
+        temp = *P; 
+        *P = (*P)->prox; 
+        free (temp); 
+    } 
+}
+
+vertice Topo (pilha P) {
+    if (! Vazia(P)) return P->elem;
+}
+
+void InicPilha (pilha *P) { *P = NULL; }
+
+logic Vazia (pilha P) {
+	if (P == NULL) return TRUE; 
+    else return FALSE; 
+}
+
+void Travessia (Grafo *G)  {
+	vertice v;
+	InicPilha (&P); aciclico = TRUE;
+	for (v = 1; v <= G->nvert; v++)
+		G->EspacoVertices[v].visit = FALSE;
+	cont = 0;
+	for (v = 1; v <= G->nvert && aciclico; v++)
+		if  (G->EspacoVertices[v].visit == FALSE)
+			BuscaProf (v, G);
+}
+void BuscaProf (vertice v, Grafo *G)  {
+	CelulaAdj *p;
+	G->EspacoVertices[v].visit = TRUE;  cont++;
+	G->EspacoVertices[v].nvisit = cont;
+	Empilhar (v, &P);
+	p = G->EspacoVertices[v].listadj;
+	while (p != NULL && aciclico)  {
+		if (G->EspacoVertices[p->vert].visit == FALSE)
+			BuscaProf(p->vert, G);
+		else if (Procurar (p->vert, &P) == TRUE)
+			aciclico = FALSE;
+		p = p->prox;
+	}
+	Desempilhar (&P);
+	Empilhar (v, &topol);
+}
+logic Procurar (vertice x, pilha *P)
+{
+	noh *percorre;
+	percorre = *P;
+
+	while (percorre != NULL)
+	{
+		if (percorre->elem == x) return TRUE;
+		percorre = percorre->prox;
+	}
+
+	return FALSE;
+}
+
+// Ordenação topológica
+void OrdTopologica (pilha x){
+	while (!Vazia(x))
+	{
+		printf ("%3d", x->elem);
+		Desempilhar (&x);
+	}
+}
+
 
 
